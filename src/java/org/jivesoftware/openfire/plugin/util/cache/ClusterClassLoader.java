a50a7e1602414317f8057b4fcf3333e439d3c59f
GregDThomas <greg.d.thomas@gmail.com>
2019-12-18 10:27:52 +0000

    Remove some code that was used to maintain backward compatibility. (#36)
    As a side effect improve ClusteredCacheFactory

--- src/java/org/jivesoftware/openfire/plugin/util/cache/ClusterClassLoader.java
+++ src/java/org/jivesoftware/openfire/plugin/util/cache/ClusterClassLoader.java
@@ -20,12 +20,16 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginClassLoader;
 import org.jivesoftware.openfire.container.PluginManager;
+import org.jivesoftware.openfire.plugin.HazelcastPlugin;
 import org.jivesoftware.util.JiveGlobals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,33 +38,34 @@ import org.slf4j.LoggerFactory;
  * Class loader to be used by Openfire to load classes that live in the Hazelcast plugin,
  * the Openfire core and also classes defined in other plugins. With this new class loader
  * plugins can now make use of hazelcast.<p>
- *
+ * <p>
  * However, there is a catch with this class loader. Plugins that define the same class name
  * (i.e. package and class name) will have a problem if they try to send that class through
  * the cluster. Hazelcast will deserialize the class and will use the first class definition
  * found in the list of plugins.<p>
- *
+ * <p>
  * The sequence of search for this class loader is first check the hazelcast plugin that
  * includes checking the Openfire core. If not found then try with the other plugins.
  *
  * @author Tom Evans
  * @author Gaston Dombiak
- * // TODO: (Greg 2019-01-25) Repalce use of deprecated pluginManager.getPluginDirectory(plugin) when OF 4.0.x support is no longer required
  */
 public class ClusterClassLoader extends ClassLoader {
-    
-    private static Logger logger = LoggerFactory.getLogger(ClusterClassLoader.class);
-    
-    private static final String HAZELCAST_CONFIG_DIR = JiveGlobals.getProperty(
-            "hazelcast.config.xml.directory", JiveGlobals.getHomeDirectory()
-                    + "/conf");
 
-    private PluginClassLoader hazelcastClassloader;
+    private static Logger logger = LoggerFactory.getLogger(ClusterClassLoader.class);
+
+    private static final String HAZELCAST_CONFIG_DIR = JiveGlobals.getProperty(
+        "hazelcast.config.xml.directory", JiveGlobals.getHomeDirectory()
+            + "/conf");
+    private static final PluginManager pluginManager = XMPPServer.getInstance().getPluginManager();
+
+    private final PluginClassLoader hazelcastClassloader;
 
     ClusterClassLoader() {
-        Plugin plugin = XMPPServer.getInstance().getPluginManager().getPlugin("hazelcast");
-        hazelcastClassloader = XMPPServer.getInstance().getPluginManager().getPluginClassloader(plugin);
-        
+        Plugin plugin = pluginManager.getPluginByName(HazelcastPlugin.PLUGIN_NAME)
+            .orElseThrow(() -> new IllegalStateException("Unable to find the Hazelcast plugin - name=" + HazelcastPlugin.PLUGIN_NAME));
+        hazelcastClassloader = pluginManager.getPluginClassloader(plugin);
+
         // this is meant to allow loading configuration files from outside the plugin JAR file
         File confFolder = new File(HAZELCAST_CONFIG_DIR);
         try {
@@ -72,74 +77,62 @@ public class ClusterClassLoader extends ClassLoader {
     }
 
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        try {
-            return hazelcastClassloader.loadClass(name);
-        }
-        catch (ClassNotFoundException e) {
-            PluginManager pluginManager = XMPPServer.getInstance().getPluginManager();
-            for (Plugin plugin : pluginManager.getPlugins()) {
-                String pluginName = pluginManager.getPluginDirectory(plugin).getName();
-                if ("hazelcast".equals(pluginName) || "admin".equals(pluginName)) {
-                    continue;
-                }
-                PluginClassLoader pluginClassloader = pluginManager.getPluginClassloader(plugin);
+        return getPluginClassLoaders()
+            .map(classLoader -> {
                 try {
-                    return pluginClassloader.loadClass(name);
+                    return classLoader.loadClass(name);
+                } catch (final ClassNotFoundException ignored) {
+                    return null;
                 }
-                catch (ClassNotFoundException e1) {
-                    // Do nothing. Continue to the next plugin
-                }
-            }
-        }
-        throw new ClassNotFoundException(name);
+            })
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElseThrow(() -> new ClassNotFoundException(name));
+    }
+
+    /**
+     * Returns a stream of plugin class loaders. The order of the stream is;
+     * <ol>
+     *     <li>The Hazelcast plugin class loader - which also includes the Openfire class loader too.
+     *     This is first as, in general, most classes will belong to the Openfire class loader</li>
+     *     <li>All the other plugins, in the order determined by the plugin manager,
+     *     excluding the admin and Hazelcast plugins.</li>
+     * </ol>
+     *
+     * @return a stream of class loaders
+     */
+    private Stream<ClassLoader> getPluginClassLoaders() {
+        return Stream.concat(Stream.of(hazelcastClassloader),
+            pluginManager.getMetadataExtractedPlugins().values()
+            .stream()
+            // Filter out the admin and Hazelcast plugins
+            .filter(pluginMetadata -> !pluginMetadata.getCanonicalName().equals("admin") &&
+                !pluginMetadata.getName().equals(HazelcastPlugin.PLUGIN_NAME))
+            .map(pluginMetadata -> pluginManager.getPluginByName(pluginMetadata.getName()))
+            .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
+            .map(pluginManager::getPluginClassloader));
     }
 
     public URL getResource(String name) {
-        URL resource = hazelcastClassloader.getResource(name);
-        if (resource == null) {
-            PluginManager pluginManager = XMPPServer.getInstance().getPluginManager();
-            for (Plugin plugin : pluginManager.getPlugins()) {
-                String pluginName = pluginManager.getPluginDirectory(plugin).getName();
-                if ("hazelcast".equals(pluginName) || "admin".equals(pluginName)) {
-                    continue;
-                }
-                PluginClassLoader pluginClassloader = pluginManager.getPluginClassloader(plugin);
-                resource = pluginClassloader.getResource(name);
-                if (resource != null) {
-                    return resource;
-                }
-            }
-        }
-        return resource;
+        return getPluginClassLoaders()
+            .map(pluginClassLoader -> pluginClassLoader.getResource(name))
+            .filter(Objects::nonNull)
+            .findFirst()
+            .orElse(null);
     }
 
     public Enumeration<URL> getResources(String name) {
-        Enumeration<URL> answer = null;
-        try {
-            answer = hazelcastClassloader.getResources(name);
-        }
-        catch (IOException e) {
-            // Ignore
-        }
-        if (answer == null || !answer.hasMoreElements()) {
-            PluginManager pluginManager = XMPPServer.getInstance().getPluginManager();
-            for (Plugin plugin : pluginManager.getPlugins()) {
-                String pluginName = pluginManager.getPluginDirectory(plugin).getName();
-                if ("hazelcast".equals(pluginName) || "admin".equals(pluginName)) {
-                    continue;
-                }
-                PluginClassLoader pluginClassloader = pluginManager.getPluginClassloader(plugin);
+        return getPluginClassLoaders()
+            .map(pluginClassLoader -> {
                 try {
-                    answer = pluginClassloader.getResources(name);
+                    return pluginClassLoader.getResources(name);
+                } catch (final IOException e) {
+                    return null;
                 }
-                catch (IOException e) {
-                    // Ignore
-                }
-                if (answer != null && answer.hasMoreElements()) {
-                    return answer;
-                }
-            }
-        }
-        return answer;
+            })
+            .filter(Objects::nonNull)
+            .filter(enumeration -> !enumeration.hasMoreElements())
+            .findFirst()
+            .orElseGet(Collections::emptyEnumeration);
     }
 }
